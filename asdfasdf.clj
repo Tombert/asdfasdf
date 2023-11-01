807c11fa9173d3c4343297c027194bb2f454e846
Thomas Gebert <thomas@gebert.app>
2023-10-31 21:55:47 -0400

    Update

--- asdfasdf.clj
+++ asdfasdf.clj
@@ -1,1 +1,24 @@
-(println "hello world")
+(import '[eu.mihosoft.vrl.v3d STL Cube Cylinder Extrude RoundedCube Sphere Vector3d Transform])
+
+(defn move-x
+  [obj n]
+  (.transformed obj (.translateX (Transform/unity) n)))
+
+(defn move-y
+  [obj n]
+  (.transformed obj (.translateY (Transform/unity) n)))
+
+(defn union-all [& objs]
+  (reduce (fn [a b] (.union a b)) objs))
+
+(let [size 40.0
+      cube (.toCSG (Cube. size size size))
+      cylinder (.toCSG (Cylinder. (/ size 4) (/ size 2) size 80))
+      sphere (.toCSG (Sphere. (* 12.5 (/ size 20))))
+      rounded-cube (.toCSG 
+                     (doto (RoundedCube. size size size)
+                       (.cornerRadius (/ size 10))))]
+  (union-all 
+	    cube
+	)
+)
